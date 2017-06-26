f349493bd1fce27b02405515cf9d396554ea1f9f
Anthony Long <anthonylong@bs2-169-dhcp-207.bio.uci.edu>
2017-06-26 13:15:12 -0700

    adding track upper left corner helper script

---
+++ UserPrograms/test_upper_left_corners.py
@@ -0,0 +1,34 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK
+BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK
+BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK
+BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK  BLANK
+"""
+#   2       3       4       5       6       7      8      9
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+
+CurrentTipPosition = 1																	
+myvol = 300
+#  1 = UL of BoxA, 2 = UR of BoxA, 3 = LL of BoxA, etc.
+OffsetDict={1: 'UL', 2: 'UR', 3: 'LL', 4: 'LR'}
+# read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+
+# ...Plate(row,column)
+defineCornersOf24WellPlates(0,2)
+defineCornersOf24WellPlates(0,3)
+
+position(0,0)
+ShutDownRobot(DOEZ=False)

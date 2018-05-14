cd8c064a4f43184649796b8e579f5ee488867f46
rlinder02 <rlinder02@gmail.com>
2018-05-14 15:10:26 -0700

    changed frozen stock transfer to test

--- UserPrograms/ASE/FrozenStock_Transfer_4.py
+++ UserPrograms/ASE/FrozenStock_Transfer_4.py
@@ -17,35 +17,36 @@ SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
 #   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
 ##################################
 
-myvol = 100
+
 #  1 = UL of BoxA, 2 = UR of BoxA, 3 = LL of BoxA, etc.
 OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
 #  read in deck, etc
 DefineDeck(deck)
 printDeck()
 InitializeRobot()
-CurrentTipPosition = 1																	
+CurrentTipPosition = 4																	
 
-for row in [0,1,2,3]:
-        for offset in [0,1,2,3]:
+### change back to row in [0,1,2,3] and offset in [0,1,2,3] and currenttipposition 1
+for row in [0]:
+        for offset in [3]:
 		
 		CurrentTipPosition = retrieveTips(CurrentTipPosition)
 
 		# from SW96 to DW96
 		position(row,2, position = OffsetDict[offset])
-		aspirate(200,depth=80,speed=50,mix=0)
+		aspirate(150,depth=80,speed=50,mix=0)
 		position(row,3, position = OffsetDict[offset])
-		dispense(myvol, depth=96, speed=50)
+		dispense(150, depth=96, speed=50)
 		
 		# initial mix
 		position(row,2, position = OffsetDict[offset])
-		mix(100,96,100,5)
+		mix(75,97,100,5)
 		
 		# from SW96 to DW96
 		position(row,2, position = OffsetDict[offset])
-		aspirate(myvol,depth=97,speed=50,mix=0)
+		aspirate(75,depth=97,speed=50,mix=0)
 		position(row,3, position = OffsetDict[offset])
-		dispense(myvol, depth=96, speed=50)
+		dispense(75, depth=96, speed=50)
 		
 		disposeTips()
 		

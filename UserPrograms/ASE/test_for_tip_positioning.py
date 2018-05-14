bd7ed6e2e61ac10ac802417957531584304c60af
rlinder02 <rlinder02@gmail.com>
2018-05-14 14:56:14 -0700

    changed x

--- UserPrograms/ASE/test_for_tip_positioning.py
+++ UserPrograms/ASE/test_for_tip_positioning.py
@@ -26,7 +26,7 @@ CurrentTipPosition = 4
 
 CurrentTipPosition = retrieveTips(CurrentTipPosition)
 
-for col in [6,7,8,9]:
+for col in [8,9]:
 	for row in [0,3]:		
 		position(row,col, position = OffsetDict[3])
 		mix(100,50,100,1)

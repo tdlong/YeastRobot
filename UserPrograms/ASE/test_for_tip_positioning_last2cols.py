5f318ed70c7bdbdadd2bee5293a7271c450407bb
rlinder02 <rlinder02@gmail.com>
2018-05-24 08:49:06 -0700

    only cols 8 and 9

---
+++ UserPrograms/ASE/test_for_tip_positioning_last2cols.py
@@ -0,0 +1,38 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+DW96W DW96W DW96W DW96W DW96W DW96W DW96W DW96W
+DW96W DW96W DW96W DW96W DW96W DW96W DW96W DW96W
+DW96W DW96W DW96W DW96W DW96W DW96W DW96W DW96W
+DW96W DW96W DW96W DW96W DW96W DW96W DW96W DW96W
+"""
+#   2       3       4       5       6
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+#  1 = UL of BoxA, 2 = UR of BoxA, 3 = LL of BoxA, etc.
+OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
+#  read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+CurrentTipPosition = 1																	
+
+CurrentTipPosition = retrieveTips(CurrentTipPosition)
+
+for col in [8,9]:
+	for row in [0,1,2,3]:		
+		position(row,col, position = OffsetDict[3])
+		mix(100,90,100,1)
+		
+position(0,0)
+ShutDownRobot()
+quit()
+
+

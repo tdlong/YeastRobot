9b949ded75d94efc44b153a8c3f7b879d92625d8
rlinder02 <rlinder02@gmail.com>
2018-05-23 18:31:02 -0700

    tip depth

--- PythonLibrary/CellClass.py
+++ PythonLibrary/CellClass.py
@@ -146,7 +146,7 @@ plateInfo = {\
     'surfDepth': 14200,\
     'safeDepth':universalSafeHeight,\
     'maxDepth': None,\
-    'tipAttachDepth': 17110,\
+    'tipAttachDepth': 17140,\
     'ejectDepth': None,\
     'wellDist': wellDist, \
     'deepWellOffset':False,\

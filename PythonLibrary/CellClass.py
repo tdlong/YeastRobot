d7723004e6cc0b745adfa5f1b183cb3038c8efa0
rlinder02 <rlinder02@gmail.com>
2018-05-25 17:01:39 -0700

    tip box c

--- PythonLibrary/CellClass.py
+++ PythonLibrary/CellClass.py
@@ -170,7 +170,7 @@ plateInfo = {\
     'surfDepth': 14200,\
     'safeDepth':universalSafeHeight,\
     'maxDepth': None,\
-    'tipAttachDepth': 17085,\
+    'tipAttachDepth': 17095,\
     'ejectDepth': None,\
     'wellDist': wellDist, \
     'deepWellOffset':False,\

7d519b9c2e3c01775da757f438df1d280327660b
Tony <tdlong@uci.edu>
2018-06-11 13:43:19 -0700

    disposeTips distance -> 800

--- PythonLibrary/RobotControl.py
+++ PythonLibrary/RobotControl.py
@@ -592,7 +592,7 @@ def disposeTips():
 	userPause()
 	print('Disposing Tips')
 #	distance = 780
-	distance = 740   
+	distance = 800   
 	# #mm to move * 39.5 steps/mm
 	# height of hook entrance = hookEntrance
 	# height of tip dispense = tipDispenseDepth

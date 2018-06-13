b2132ad624fc4a745d16cecbd9577feb51c5c120
Tony <tdlong@uci.edu>
2018-06-13 12:35:59 -0700

    new seating heights for tips

--- PythonLibrary/CellClass.py
+++ PythonLibrary/CellClass.py
@@ -41,7 +41,7 @@ STDalignmentDepth = s.STDalignmentDepth
 STDtipAttach = s.STDtipAttach
 universalSafeHeight = s.universalSafeHeight; #update to optimize speed, minimizing travel distance
 wellDist = 350
-UniversalTipAttachDepth=17200
+UniversalTipAttachDepth=17225
 
 plateInfo = {\
 'DW96P': \

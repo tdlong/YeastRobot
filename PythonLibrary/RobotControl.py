b87d906597c9bfc19bcaee62c4bdd0341b03baa2
rlinder02 <rlinder02@gmail.com>
2018-06-13 12:24:11 -0700

    3 times seatimg

--- PythonLibrary/RobotControl.py
+++ PythonLibrary/RobotControl.py
@@ -822,6 +822,10 @@ def retrieveTips(CurrentTipPosition, align="False"):
 	else:
 		VLMX_SetSpeed(ZMotor, 4*ZSpeedSlow)
 		VLMX_GoTo_A(ZMotor, matrix[currentx][currenty].tipAttachDepth)
+		VLMX_GoTo_A(ZMotor, matrix[currentx][currenty].tipAttachDepth - 400)
+		VLMX_GoTo_A(ZMotor, matrix[currentx][currenty].tipAttachDepth)
+		VLMX_GoTo_A(ZMotor, matrix[currentx][currenty].tipAttachDepth - 400)
+		VLMX_GoTo_A(ZMotor, matrix[currentx][currenty].tipAttachDepth)
 		VLMX_SetSpeed(ZMotor, ZSpeedFast)
 		VLMX_GoTo_A(ZMotor, matrix[currentx][currenty].safeDepth)
 	

492d1c26a1986ae93e6d8a54cc78c426a8850eb3
rlinder02 <rlinder02@gmail.com>
2018-07-09 13:57:04 -0700

    keeping 96DWPC at 99

--- UserPrograms/ASE/Mon_Add_Tween20_3.py
+++ UserPrograms/ASE/Mon_Add_Tween20_3.py
@@ -49,7 +49,7 @@ for row in [0,1,2]:
 		dispense(250, depth=96, speed=90)
 
 		position(row,4,position = OffsetDict[offset])
-		aspirate(250,depth=96,speed=40, mix=0)
+		aspirate(250,depth=99,speed=40, mix=0)
 		position(row,2,position = OffsetDict[offset])
 		dispense(250, depth=85, speed=90)
 		

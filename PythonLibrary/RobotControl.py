d9b8b5a78d68eac165b9cca907b4dbe74c0b3888
rlinder02 <rlinder02@gmail.com>
2018-07-09 20:43:03 -0700

    eject

--- PythonLibrary/RobotControl.py
+++ PythonLibrary/RobotControl.py
@@ -605,7 +605,7 @@ def disposeTips():
 	userPause()
 	print('Disposing Tips')
 #	distance = 680
-	distance = 710   
+	distance = 730   
 	# #mm to move * 39.5 steps/mm
 	# height of hook entrance = hookEntrance
 	# height of tip dispense = tipDispenseDepth

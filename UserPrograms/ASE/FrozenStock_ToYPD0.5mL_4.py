800b3de74ffb57ca60caea8cd02e9149cb240a43
rlinder02 <rlinder02@gmail.com>
2018-07-17 13:37:29 -0700

    adjusted depth

--- UserPrograms/ASE/FrozenStock_ToYPD0.5mL_4.py
+++ UserPrograms/ASE/FrozenStock_ToYPD0.5mL_4.py
@@ -31,23 +31,23 @@ for row in [0,1,2,3]:
 		
 		CurrentTipPosition = retrieveTips(CurrentTipPosition)
 		extraSeatTips()
-		adjusted_depth = 98 + row
+		adjusted_depth = 99 + row
 		#aspirate 300 ul of H2O (C3) -> discard to DW96W at C4
 		position(row,3, position = OffsetDict[offset])
 		aspirate(myvol,depth=adjusted_depth,speed=50,mix=0)
 		position(row,4, position = OffsetDict[offset])
-		dispense(myvol, depth=95, speed=50)
+		dispense(myvol, depth=adjusted_depth, speed=50)
 
 		#transfer 2x250 ul of YPDa(C6) -> resuspend pellet (C3)
 		position(row,6, position = OffsetDict[offset])
-		aspirate(250,depth=99,speed=50,mix=0)
+		aspirate(250,depth=adjusted_depth,speed=50,mix=0)
 		position(row,3, position = OffsetDict[offset])
-		dispense(250, depth=99, speed=50)
+		dispense(250, depth=adjusted_depth, speed=50)
 
 		position(row,6, position = OffsetDict[offset])
-		aspirate(250,depth=adjusted_depth + 2,speed=50,mix=0)
+		aspirate(250,depth=adjusted_depth + 1,speed=50,mix=0)
 		position(row,3, position = OffsetDict[offset])
-		dispense(250, depth=85, speed=50)
+		dispense(250, depth=adjusted_depth, speed=50)
 
 		mix(300,adjusted_depth,100,5)
 		

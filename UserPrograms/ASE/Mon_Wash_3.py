9d663e9cc506c6e5e975cc50c81547f3dcccc41a
rlinder02 <rlinder02@gmail.com>
2018-07-30 14:16:57 -0700

    changed depths

--- UserPrograms/ASE/Mon_Wash_3.py
+++ UserPrograms/ASE/Mon_Wash_3.py
@@ -41,7 +41,7 @@ for row in [0,1,2]:
 		position(row,2,position = OffsetDict[offset])
 		aspirate(200,depth=adjusted_depth,speed=50, mix=0)
 		position(row,3, position = OffsetDict[offset])
-		dispense(200, depth=90, speed=50)
+		dispense(200, depth=adjusted_depth - 4, speed=50)
 		
 		# pick up 300ul of YPD from C4, add to C2
 		position(row,4,position = OffsetDict[offset])

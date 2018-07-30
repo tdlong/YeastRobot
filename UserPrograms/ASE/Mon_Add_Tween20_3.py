c84630993c7135256b9073c45e63f4616dc99054
rlinder02 <rlinder02@gmail.com>
2018-07-30 13:25:07 -0700

    reduced dispension speed of tween 20

--- UserPrograms/ASE/Mon_Add_Tween20_3.py
+++ UserPrograms/ASE/Mon_Add_Tween20_3.py
@@ -46,12 +46,12 @@ for row in [0,1,2]:
 		position(row,4,position = OffsetDict[offset])
 		aspirate(250,depth=adjusted_depth + 1,speed=40, mix=0)
 		position(row,2,position = OffsetDict[offset])
-		dispense(250, depth=96, speed=90)
+		dispense(250, depth=96, speed=40)
 
 		position(row,4,position = OffsetDict[offset])
 		aspirate(250,depth=adjusted_depth + 1,speed=40, mix=0)
 		position(row,2,position = OffsetDict[offset])
-		dispense(250, depth=85, speed=90)
+		dispense(250, depth=85, speed=40)
 		
 		# discard tips
 		#disposeTips()

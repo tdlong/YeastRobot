c5984456a2a1536eb61aa351a47ef22cc1f8617d
rlinder02 <rlinder02@gmail.com>
2018-08-03 13:55:37 -0700

    adjusted depths

--- UserPrograms/ASE/Friday_Wash_16.py
+++ UserPrograms/ASE/Friday_Wash_16.py
@@ -30,7 +30,7 @@ for col in [2,3,4,5]:
 	for row in [0,1,2,3]:
 		CurrentTipPosition = retrieveTips(CurrentTipPosition)
 		extraSeatTips()
-		adjusted_depth = 97 + row*2
+		adjusted_depth = 95 + row*2
 
 		# transfer 3X 330 from DW24P to WASTE#1 at col 6
 		position(row,col)

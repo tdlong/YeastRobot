1b46439f6ffc716d0a8e333141a9673044101f9b
rlinder02 <rlinder02@gmail.com>
2019-02-12 14:40:38 -0800

    fixed

--- UserPrograms/ASE/Restocking_frozenstock_4v2.py
+++ UserPrograms/ASE/Restocking_frozenstock_4v2.py
@@ -8,10 +8,10 @@ from RobotControl import *
 ###  Define Deck Layout
 #################################
 deck="""\
-SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
-SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
-SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
-SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
+DW96W  SW96P  SW96P  BLANK  BLANK  BLANK  BLANK  BLANK
+DW96W  SW96P  SW96P  BLANK  BLANK  BLANK  BLANK  BLANK
+DW96W  SW96P  SW96P  BLANK  BLANK  BLANK  BLANK  BLANK
+DW96W  SW96P  SW96P  BLANK  BLANK  BLANK  BLANK  BLANK
 """
 #   2       3       4       5       6
 #   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1

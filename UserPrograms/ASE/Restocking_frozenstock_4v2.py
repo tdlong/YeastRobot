58ca13dcb7dace1f3bc7e7450fb35d3041d26b75
rlinder02 <rlinder02@gmail.com>
2019-02-12 14:37:54 -0800

    debugged

---
+++ UserPrograms/ASE/Restocking_frozenstock_4v2.py
@@ -0,0 +1,56 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
+SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
+SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
+SW96P  DW96W  DW96W  DW96W  DW96W  BLANK  BLANK  BLANK
+"""
+#   2       3       4       5       6
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+
+
+#  1 = UL of BoxA, 2 = UR of BoxA, 3 = LL of BoxA, etc.
+OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
+#  read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+CurrentTipPosition = 1																	
+
+for row in [0,1,2,3]:
+        for offset in [0,1,2,3]:
+		
+		CurrentTipPosition = retrieveTips(CurrentTipPosition)
+		extraSeatTips()
+		adjusted_depth = 96 + row
+		
+		# initial mix
+		position(row,2, position = OffsetDict[offset])
+		mix(320,adjusted_depth + 1,100,10)
+		
+		# from DW96 to SW96
+		position(row,2, position = OffsetDict[offset])
+		aspirate(140,depth=adjusted_depth,speed=50,mix=0)
+		position(row,3, position = OffsetDict[offset])
+		moveDispense(140, startdepth = 95, enddepth=60, speed = 50)
+		
+		position(row,2, position = OffsetDict[offset])
+		aspirate(140,depth=adjusted_depth,speed=50,mix=2)
+		position(row,4, position = OffsetDict[offset])
+		moveDispense(140, startdepth = 95, enddepth=60, speed = 50)
+		#disposeTips()
+		manualDisposeTips()
+
+position(0,0)
+ShutDownRobot()
+quit()
+

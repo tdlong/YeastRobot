75717311b3b3572e51b7cc82aa68ec1bca2c8226
rlinder02 <rlinder02@gmail.com>
2019-03-20 16:19:56 -0700

    Update DXQTL02_24to96_16.py

--- UserPrograms/ASE/DXQTL02_24to96_16.py
+++ UserPrograms/ASE/DXQTL02_24to96_16.py
@@ -23,9 +23,9 @@ OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
 DefineDeck(deck)
 printDeck()
 InitializeRobot()
-CurrentTipPosition = 1
+CurrentTipPosition = 3
 
-for col in [2,3,4,5]:
+for col in [3,4,5]:
 	for row in [0,3]:
 		CurrentTipPosition = retrieveTips(CurrentTipPosition)
 		extraSeatTips()
@@ -33,7 +33,7 @@ for col in [2,3,4,5]:
 
 		# pick up 3 * 320ul of 1xTE from C7, add to C2-5
 		position(col-2,7,position = OffsetDict[row])
-		aspirate(320,depth= adjusted_depth-25,speed=75, mix=0)
+		aspirate(320,depth= adjusted_depth-35,speed=75, mix=0)
 		position(row,col)
 		dispense(320, depth=adjusted_depth, speed=100)
 
@@ -59,18 +59,18 @@ for col in [2,3,4,5]:
 		position(row,col)
 		aspirate(320, depth=adjusted_depth+6,speed=75, mix=0)
 		position(col-2,6,position = OffsetDict[row])
-		dispense(320, depth=adjusted_depth+1, speed=50)
+		dispense(320, depth=adjusted_depth-2, speed=50)
 		position(row,col)
 		mix(320,adjusted_depth+6,100,2)
 		position(row,col)
 		aspirate(320, depth=adjusted_depth + 6,speed=75, mix=0)
 		position(col-2,6,position = OffsetDict[row])
-		dispense(320, depth=adjusted_depth+1, speed=50)
+		dispense(320, depth=adjusted_depth-2, speed=50)
 		position(row,col)
 		mix(320,adjusted_depth+7,100,2)
 		aspirate(320, depth=adjusted_depth + 7,speed=75, mix=0)
 		position(col-2,6,position = OffsetDict[row])
-		dispense(320, depth=adjusted_depth+1, speed=50)
+		dispense(320, depth=adjusted_depth-2, speed=50)
 					
 		#disposeTips()
 		manualDisposeTips()

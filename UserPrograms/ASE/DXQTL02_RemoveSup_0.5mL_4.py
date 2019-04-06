795d0c2945513d16f0ae6f4e340122314a06654b
rlinder02 <rlinder02@gmail.com>
2019-04-05 21:30:02 -0700

    Update DXQTL02_RemoveSup_0.5mL_4.py

--- UserPrograms/ASE/DXQTL02_RemoveSup_0.5mL_4.py
+++ UserPrograms/ASE/DXQTL02_RemoveSup_0.5mL_4.py
@@ -39,7 +39,7 @@ for row in [0,1,2]:
 		position(row,3, position = OffsetDict[offset])
 		dispense(250, depth=adjusted_depth - 10, speed=50)
 		position(row,2,position = OffsetDict[offset])
-		aspirate(250,depth=adjusted_depth,speed=50, mix=0)
+		aspirate(250,depth=adjusted_depth + 1,speed=50, mix=0)
 		position(row,3, position = OffsetDict[offset])
 		dispense(250, depth=adjusted_depth - 14, speed=50)
 		# discard tips

166c50a52ac621c74d3b45942e7b04275da8124b
rlinder02 <rlinder02@gmail.com>
2019-06-04 10:38:50 -0700

    Update Recovery_4to16_and_frozenstock.py

--- UserPrograms/ASE/Recovery_4to16_and_frozenstock.py
+++ UserPrograms/ASE/Recovery_4to16_and_frozenstock.py
@@ -52,7 +52,7 @@ for row in [0,1,2,3]:
 
         	position(row,2,position = OffsetDict[offset])
         	aspirate(140,depth=adjusted_depth,speed=50, mix=2)
-        	position(row, 8, position = OffsetDict[offset])
+        	position(row, 9, position = OffsetDict[offset])
         	moveDispense(140, startdepth = 95, enddepth=60, speed = 50)
 		
 		#disposeTips()

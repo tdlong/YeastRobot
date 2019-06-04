bcf64538468e93dee4260ee0202b39b223fdf7a9
rlinder02 <rlinder02@gmail.com>
2019-06-04 09:38:30 -0700

    Update Recovery_4to16_and_frozenstock.py

--- UserPrograms/ASE/Recovery_4to16_and_frozenstock.py
+++ UserPrograms/ASE/Recovery_4to16_and_frozenstock.py
@@ -8,10 +8,10 @@ from RobotControl import *
 ###  Define Deck Layout
 #################################
 deck="""\
-DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P
-DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P
-DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P
-DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P
+DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P  SW96P
+DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P  SW96P
+DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P  SW96P
+DW96W  SW24P  SW24P  SW24P  SW24P  SW96P  SW96P  SW96P
 """
 #   2       3       4       5       6
 #   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
@@ -29,20 +29,25 @@ for row in [0,1,2,3]:
 
 		CurrentTipPosition = retrieveTips(CurrentTipPosition)
 		extraSeatTips()
-		adjusted_depth = 94 + row
+		adjusted_depth = 96 + row
 		# initial mix
 		position(row,2,position = OffsetDict[offset])
-		mix(320,97,100,10)
+		mix(320,adjusted_depth,100,10)
 
 		# from DW96W to SW24P
 		position(row,2,position = OffsetDict[offset])
 		aspirate(140, depth=adjusted_depth, speed=50, mix=0)
 		position(offset,row+3)
-		dispense(140, depth=adjusted_depth + 5, speed=50)
+		dispense(140, depth=adjusted_depth + 3, speed=50)
         
         	position(row,2,position = OffsetDict[offset])
         	aspirate(140,depth=adjusted_depth,speed=50, mix=2)
         	position(row, 7, position = OffsetDict[offset])
+        	moveDispense(140, startdepth = 95, enddepth=60, speed = 50)
+
+        	position(row,2,position = OffsetDict[offset])
+        	aspirate(140,depth=adjusted_depth,speed=50, mix=2)
+        	position(row, 8, position = OffsetDict[offset])
         	moveDispense(140, startdepth = 95, enddepth=60, speed = 50)
 
         	position(row,2,position = OffsetDict[offset])

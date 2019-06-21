a7e7e47c2fd030abd51868173344711ba826bf10
Robert Linder <rlinder02@gmail.com>
2019-06-21 13:01:15 -0700

    Create Friday_16_temp.py

---
+++ UserPrograms/ASE/Friday_16_temp.py
@@ -0,0 +1,71 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+SW24P  SW24P  SW24P  SW24P  DW24P  DW24P  DW24P  DW24P 
+SW24P  SW24P  SW24P  SW24P  DW24P  DW24P  DW24P  DW24P 
+SW24P  SW24P  SW24P  SW24P  DW24P  DW24P  DW24P  DW24P 
+SW24P  SW24P  SW24P  SW24P  DW24P  DW24P  DW24P  DW24P 
+"""
+#   2       3       4       5       6
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+
+myvol = 140
+#  1 = UL of BoxA, 2 = UR of BoxA, 3 = LL of BoxA, etc.
+OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
+#  read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+CurrentTipPosition = 8																	
+
+for col in [3]:
+	for row in [3]:
+		CurrentTipPosition = retrieveTips(CurrentTipPosition)
+		extraSeatTips()
+		adjusted_depth = 92 + row*2
+		
+		# initial mix
+		position(row,col)
+		mix(300,adjusted_depth + 3,100,5)
+		
+		# from SW24 to DW24  320 ul X3 = 990ul
+		# this is the most we can get from a plate when we start with 1.4ml
+		for i in [1,2,3]:
+			position(row,col)
+			aspirate(320,depth=adjusted_depth + 9,speed=50, mix=0)
+			position(row,col+4,position = OffsetDict[row])
+			dispense(320, depth=adjusted_depth + 3, speed=50)
+		#disposeTips()
+		manualDisposeTips()
+
+for col in [4,5]:
+	for row in [0,1,2,3]:
+		CurrentTipPosition = retrieveTips(CurrentTipPosition)
+		extraSeatTips()
+		adjusted_depth = 92 + row*2
+		
+		# initial mix
+		position(row,col)
+		mix(300,adjusted_depth + 3,100,5)
+		
+		# from SW24 to DW24  320 ul X3 = 990ul
+		# this is the most we can get from a plate when we start with 1.4ml
+		for i in [1,2,3]:
+			position(row,col)
+			aspirate(320,depth=adjusted_depth + 9,speed=50, mix=0)
+			position(row,col+4,position = OffsetDict[row])
+			dispense(320, depth=adjusted_depth + 3, speed=50)
+		#disposeTips()
+		manualDisposeTips()
+position(0,0)
+ShutDownRobot()
+quit()
+

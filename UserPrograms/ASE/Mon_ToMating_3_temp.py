227dac7c5c4b587918debe139297c2c5f728c3de
Robert Linder <rlinder02@gmail.com>
2019-08-12 18:55:08 -0700

    Create Mon_ToMating_3_temp.py

---
+++ UserPrograms/ASE/Mon_ToMating_3_temp.py
@@ -0,0 +1,56 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+DW96W  DW96W  DW96W BLANK BLANK BLANK BLANK
+DW96W  DW96W  DW96W BLANK BLANK BLANK BLANK
+DW96W  DW96W  DW96W BLANK BLANK BLANK BLANK 
+BLANK  BLANK  BLANK BLANK BLANK BLANK BLANK
+"""
+#   2       3       4       5       6
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+
+OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
+#  read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+CurrentTipPosition = 9
+
+
+# eventually row in 0,1,2,3
+for row in [2]:
+	for offset in [0,1,2,3]:
+		# get tips
+		CurrentTipPosition = retrieveTips(CurrentTipPosition)
+		extraSeatTips()
+		adjusted_depth = 94 + row
+
+		#aspirate 330 ul of YPD (C2) -> discard to DW96W at C3
+		position(row,2,position = OffsetDict[offset])
+		aspirate(320,depth=adjusted_depth,speed=50, mix=0)
+		position(row,3, position = OffsetDict[offset])
+		dispense(320, depth=adjusted_depth - 8, speed=50)
+		
+			
+		# pick up 250ul of YPD from C4, add to C2
+		position(row,4,position = OffsetDict[offset])
+		aspirate(250,depth=adjusted_depth + 2,speed=50, mix=0)
+		position(row,2,position = OffsetDict[offset])
+		dispense(250, depth=adjusted_depth, speed=50)
+		
+		# discard tips
+		#disposeTips()
+		manualDisposeTips()
+
+position(0,0)
+ShutDownRobot()
+quit()
+

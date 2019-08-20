0abdf4dfbccbc80910f2c76886558c29c33b8cf7
Robert Linder <rlinder02@gmail.com>
2019-08-19 22:12:27 -0700

    temp

---
+++ UserPrograms/ASE/Mon_AsexualCleanup_4_temp_temp.py
@@ -0,0 +1,55 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+DW24P  DW96W  DW96W  DW96W  BLANK
+DW24P  BLANK  BLANK  BLANK  BLANK
+DW24P  BLANK  BLANK  BLANK  BLANK
+DW24P  BLANK  BLANK  BLANK  BLANK
+"""
+#   2       3       4       5       6
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+
+OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
+#  read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+CurrentTipPosition = 2
+
+for col in [2]:
+	for row in [1]:
+		CurrentTipPosition = retrieveTips(CurrentTipPosition)
+		extraSeatTips()
+		adjusted_depth = 95 + row*2
+
+        # transfer 300uL YPDa from DW96W reservoir at c5 to DW24P at c2
+		position(col-2,5,position = OffsetDict[row])
+		aspirate(200,depth=96+row,speed=50, mix=0)
+		position(row,col)
+		dispense(200, depth=adjusted_depth, speed=50)
+
+		# mix
+		position(row,col)
+		mix(250,adjusted_depth,100,5)
+
+		# 250 from DW24P at c2 to DW96W at c3
+		position(row,col)
+		aspirate(250,depth=adjusted_depth,speed=50, mix=0)
+		position(col-2,3,position = OffsetDict[row])
+		dispense(250, depth=96+row, speed=50)
+				
+		#disposeTips()
+		manualDisposeTips()
+
+position(0,0)
+ShutDownRobot()
+quit()
+

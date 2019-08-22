aed36fea4efd053f4ff4e99421f7b9d5dae6d0b1
Robert Linder <rlinder02@gmail.com>
2019-08-22 16:55:11 -0700

    Create Mon_DiploidSelection_4_temp.py

---
+++ UserPrograms/ASE/Mon_DiploidSelection_4_temp.py
@@ -0,0 +1,49 @@
+import sys
+#  where RobotControl.py, etc lives
+sys.path.append('/home/pi/Desktop/ADL/YeastRobot/PythonLibrary')
+from RobotControl import *
+
+
+#################################
+###  Define Deck Layout
+#################################
+deck="""\
+DW96W  DW96W  BLANK BLANK BLANK BLANK BLANK 
+DW96W  DW96W  BLANK BLANK BLANK BLANK BLANK 
+ DW96W  DW96W  BLANK BLANK BLANK BLANK BLANK  
+"""
+#   2       3       4       5       6
+#   note the 1st user defined column is "2" not zero or one, since tips are at 0 & 1
+##################################
+
+OffsetDict={0: 'UL', 1: 'UR', 2: 'LL', 3: 'LR'}
+#  read in deck, etc
+DefineDeck(deck)
+printDeck()
+InitializeRobot()
+CurrentTipPosition = 1
+
+
+# eventually row in 0,1,2,3
+for row in [3]:
+	for offset in [0,1,2,3]:
+		# get tips
+		CurrentTipPosition = retrieveTips(CurrentTipPosition)
+		extraSeatTips()
+		adjusted_depth = 96 + row
+
+		# pick up 250ul of selective media from C3, add to C2, mix
+		position(row,3,position = OffsetDict[offset])
+		aspirate(250,depth=adjusted_depth,speed=100, mix=0)
+		position(row,2,position = OffsetDict[offset])
+		dispense(250, depth=adjusted_depth, speed=100)
+		mix(320,adjusted_depth,100,10)
+		
+		# discard tips
+		#disposeTips()
+		manualDisposeTips()
+
+position(0,0)
+ShutDownRobot()
+quit()
+

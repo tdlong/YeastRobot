a915196724870af6c446375cfafe771813ff7cb1
Robert Linder <rlinder02@gmail.com>
2019-08-23 10:12:20 -0700

    Update Friday_16_temp.py

--- UserPrograms/ASE/Friday_16_temp.py
+++ UserPrograms/ASE/Friday_16_temp.py
@@ -34,7 +34,7 @@ for row in [3]:
 	elif row == 2:
 		adjusted_depth = 94 + row
 	else:
-		adjusted_depth = 95 + row
+		adjusted_depth = 96 + row
 	for col in [2,5]:
 		# initial mix
 		position(row,col)

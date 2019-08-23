7ed65d88161b1c3071939a90d7ec3b4b68a15b3b
Robert Linder <rlinder02@gmail.com>
2019-08-23 08:48:43 -0700

    Update Friday_16_temp.py

--- UserPrograms/ASE/Friday_16_temp.py
+++ UserPrograms/ASE/Friday_16_temp.py
@@ -26,8 +26,8 @@ printDeck()
 InitializeRobot()
 CurrentTipPosition = 1																	
 
-for col in [2]:
-	for row in [0,1,2,3]:
+for col in [2,4]:
+	for row in [0]:
 		CurrentTipPosition = retrieveTips(CurrentTipPosition)
 		extraSeatTips()
 		adjusted_depth = 92 + row*2
@@ -35,7 +35,7 @@ for col in [2]:
 		# initial mix
 		position(row,col)
 		mix(300,adjusted_depth + 3,100,3)
-		manualDisposeTips()
+manualDisposeTips()
 position(0,0)
 ShutDownRobot()
 quit()

606365201c690126dcaac45a59ea116ecb2e6831
rlinder02 <37880379+rlinder02@users.noreply.github.com>
2020-05-04 10:40:55 -0700

    Update Mon_Add_PBS_3.py

--- UserPrograms/ASE/Mon_Add_PBS_3.py
+++ UserPrograms/ASE/Mon_Add_PBS_3.py
@@ -43,11 +43,15 @@ for row in [0,1,2]:
 		position(row,3, position = OffsetDict[offset])
 		dispense(250, depth=adjusted_depth - 8, speed=50)
 
-		# pick up 1 * 300ul of PBS from C4, add to C2
+		# pick up 2 * 250ul of SDS from C4, add to C2
 		position(row,4,position = OffsetDict[offset])
-		aspirate(300,depth=adjusted_depth + 2,speed=50, mix=0)
+		aspirate(250,depth=adjusted_depth + 2,speed=50, mix=0)
 		position(row,2,position = OffsetDict[offset])
-		dispense(300, depth=adjusted_depth + 3, speed=100)
+		dispense(250, depth=adjusted_depth + 3, speed=100)
+		position(row,4,position = OffsetDict[offset])
+		aspirate(250,depth=adjusted_depth + 2,speed=50, mix=0)
+		position(row,2,position = OffsetDict[offset])
+		dispense(250, depth=adjusted_depth + 3, speed=100)
 		
 		# initial mix
 		position(row,2,position = OffsetDict[offset])
